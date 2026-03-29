ca425673bd976df8ad545b2b0fadd879ef1e7587
Ario Shakeri <itsario900@gmail.com>
2026-03-30 02:36:14 +0330

    Basic piece selection

---
+++ resources/piece_data.gd
@@ -0,0 +1,11 @@
+class_name PieceData
+extends Resource
+
+@export_group("Visuals")
+@export var texture: Texture2D
+@export var collision_shape: Shape2D
+
+@export_group("Stats")
+@export var knockback: float = 1.0
+@export var power: float = 1.0
+@export var defense: float = 1.0
