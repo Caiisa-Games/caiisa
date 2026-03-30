725973ab2c72ad11aa509cb516c68ef9fcbf4052
Ario Shakeri <itsario900@gmail.com>
2026-03-30 20:38:03 +0330

    Updated card display to ScrollContainer

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	$BoxContainer/QuitButton.pressed.connect(on_quit_pressed)
 
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/card.tscn")
+	get_tree().change_scene_to_file("res://scenes/piece_selection.tscn")
 
 func on_quit_pressed() -> void:
 	get_tree().quit()
