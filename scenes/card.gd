76a6d28684b949d486bd51e61ecc52ee7ae9fee8
Ario Shakeri <itsario900@gmail.com>
2026-04-08 10:24:18 +0330

    Added data to card

--- scenes/card.gd
+++ scenes/card.gd
@@ -1,1 +1,16 @@
 extends Control
+
+@export var data: PieceData
+
+@onready var texture_sprite: TextureRect = $VBoxContainer/TextureRect
+@onready var title_label: Label = $VBoxContainer/Label
+@onready var hp: ProgressBar = $VBoxContainer/ProgressBar
+
+
+func _ready() -> void:
+	if not data: return
+
+	hp.value = data.defense
+	
+	texture_sprite.texture = data.texture
+	title_label.text = data.name
