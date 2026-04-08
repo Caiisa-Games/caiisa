46812a242bbe84b2afff4441836cfa6fe72338a5
esy <esy90afy@gmail.com>
2026-04-08 04:53:20 +0330

    menu, card

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+var gh := false
 func _ready() -> void:
 	$BoxContainer/StartButton.pressed.connect(on_start_pressed)
 	$BoxContainer/QuitButton.pressed.connect(on_quit_pressed)
@@ -10,3 +11,16 @@ func on_start_pressed() -> void:
 
 func on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	if _on_credits_button_pressed:
+		$Label2.position = Vector2(375, 645)
+		create_tween().tween_property($Label2, "position", Vector2(375, -840), 25).set_trans(Tween.TRANS_SINE)     
+		$BoxContainer/creditsButton.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		$BoxContainer/StartButton.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		$BoxContainer/QuitButton.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		await get_tree().create_timer(25).timeout
+		$BoxContainer/creditsButton.mouse_filter = Control.MOUSE_FILTER_PASS
+		$BoxContainer/StartButton.mouse_filter = Control.MOUSE_FILTER_PASS
+		$BoxContainer/QuitButton.mouse_filter = Control.MOUSE_FILTER_PASS
