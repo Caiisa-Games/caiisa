58d1585b99f56509de07ec37fa2319fe45a22f9a
Ario Shakeri <itsario900@gmail.com>
2026-04-08 14:16:18 +0330

    Added piece moving

--- scenes/board.gd
+++ scenes/board.gd
@@ -8,21 +8,28 @@ var location = Vector2i(0,0)
 
 var tiles: Dictionary = {}
 var occupants: Dictionary = {}  # occupant_node -> Vector2i(grid_pos)
+var selected_tile: Node2D = null
+
+enum State { IDLE, SELECTED }
+var current_state: State = State.IDLE
 
 func generate() -> void:
 	var container = $TileContainer
-	
 	for y in range(7):
 		for x in range(7):
 			var tile = tile_scene.instantiate()
 			tile.name = "Tile_%d_%d" % [x, y]
 			tile.position = _get_iso_pos(x, y)
-			
+
 			var cell_index = y * 7 + x
-			var cell = board_data.cell_heights[cell_index]
+			var height = board_data.cell_heights[cell_index]
+
 			tile.init()
-			tile.set_height(cell)
+			tile.set_height(height)
+			tile.grid_position = Vector2i(x, y)
 			
+			tile.tile_clicked.connect(_on_tile_clicked)
+
 			container.add_child(tile)
 			tiles[Vector2i(x, y)] = tile
 			
@@ -34,26 +41,45 @@ func _get_iso_pos(x: int, y: int) -> Vector2:
 		(x - y) * 64,
 		(x + y) * 29
 	)
+
+func _on_idle_click(tile: Node2D) -> void:
+	if tile.occupant != null:
+		selected_tile = tile
+		current_state = State.SELECTED
+
+func _on_tile_clicked(grid_pos: Vector2i) -> void:
+	var tile = tiles[grid_pos]
 	
-func move_occupant(from_tile: Node2D, to_grid_pos: Vector2i, animated: bool = false) -> void:
+	match current_state:
+		State.IDLE:
+			if tile.occupant != null:
+				selected_tile = tile
+				current_state = State.SELECTED
+		
+		State.SELECTED:
+			if tile == selected_tile:
+				_deselect()
+			elif tile.occupant == null:
+				_move_occupant(selected_tile, tile)
+				_deselect()
+			else:
+				selected_tile = tile
+
+func _deselect() -> void:
+	selected_tile = null
+	current_state = State.IDLE
+	
+func _move_occupant(from_tile: Node2D, to_tile: Node2D) -> void:
 	var occupant = from_tile.occupant
-	
-	if not occupant:
-		push_error("No occupant to move!")
-		return
-	
-	if not tiles.has(to_grid_pos):
-		push_error("Invalid target position!")
-		return
-	
-	var target_tile = tiles[to_grid_pos]
+
+	from_tile.occupant = null
+	to_tile.occupant = occupant
 	
 	from_tile.clear_occupant()
 	
-	target_tile.set_occupant(occupant, 1)
-	
-	#if animated:
-		#_animate_occupant(occupant, target_tile)
+	to_tile.set_occupant(occupant, 1)
+
+	#_animate_occupant(occupant, to_tile)
 
 func _animate_occupant(occupant: Node2D, target_tile: Node2D) -> void:
 	var target_pos = target_tile.position
@@ -65,13 +91,20 @@ func _animate_occupant(occupant: Node2D, target_tile: Node2D) -> void:
 	tween.set_trans(Tween.TRANS_CUBIC)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(occupant, "position", target_pos, 0.3)
-	
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed():
-		if event.keycode == KEY_1:
-			var my_occupant = tiles[location]
-			location = Vector2i(randi() % 6, randi()%6)
-			move_occupant(my_occupant, location)
+
+func _get_tile_at_position(screen_pos: Vector2) -> Node2D:
+	for tile in tiles.values():
+		var dist = screen_pos.distance_to(tile.position)
+		if dist < 30:
+			return tile
+	return null
+
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventKey and event.is_pressed():
+		#if event.keycode == KEY_1:
+			#var my_occupant = tiles[location]
+			#location = Vector2i(randi() % 6, randi()%6)
+			#_move_occupant(my_occupant, tiles[location])
 
 func _ready() -> void:
 	generate()
