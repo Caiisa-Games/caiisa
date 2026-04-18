0e37987a9d6b6d2153639f43f81cde260edce24e
esy <esy90afy@gmail.com>
2026-04-18 21:20:10 +0330

    add setting

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -1,12 +1,32 @@
 extends Button
 
-@onready var color_rect: ColorRect = $ColorRect
+
+@onready var color_rec: ColorRect = $ColorRectf
 
 
+
+@onready var chec: CheckButton = $ColorRect/VBoxContainer/Label3/CheckButton
 
 
 func _on_pressed() -> void:
-	if color_rect.visible == false:
-		color_rect.visible = true
-	elif color_rect.visible == true:
-		color_rect.visible = false
+	if color_rec.visible == false:
+		color_rec.visible = true
+	elif color_rec.visible == true:
+		color_rec.visible = false
+
+
+func _on_check_button_pressed() -> void:
+	$"../Label".visible = true
+	$ColorRectf/VBoxContainer.visible = false
+	$ColorRectf/VBoxContainer2.visible = true
+
+func _ready() -> void:
+	if $".".mouse_filter == MOUSE_FILTER_IGNORE:
+		color_rec.visible = false
+	elif $".".mouse_filter == MOUSE_FILTER_PASS:
+		color_rec.visible = true
+
+
+func _on_check_button_6_pressed() -> void:
+	$ColorRectf/VBoxContainer.visible = true
+	$ColorRectf/VBoxContainer2.visible = false
