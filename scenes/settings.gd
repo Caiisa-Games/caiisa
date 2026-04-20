01502ab11eaf61e812cfcebee572e5d32f1dcf47
esy <esy90afy@gmail.com>
2026-04-20 16:16:08 +0330

    add setting

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -25,8 +25,20 @@ func _ready() -> void:
 		color_rec.visible = false
 	elif $".".mouse_filter == MOUSE_FILTER_PASS:
 		color_rec.visible = true
-
+	
+	#self.connect("item_se")
 
 func _on_check_button_6_pressed() -> void:
 	$ColorRectf/VBoxContainer.visible = true
 	$ColorRectf/VBoxContainer2.visible = false
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	if index == 0:
+		$ColorRectf/VBoxContainer.visible = true
+		$ColorRectf/VBoxContainer2.visible = false
+		$"../Label".visible = false
+	elif index == 1:
+		$"../Label".visible = true
+		$ColorRectf/VBoxContainer.visible = false
+		$ColorRectf/VBoxContainer2.visible = true
