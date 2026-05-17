085e49db8094f4a418f0d2d33e8ec8b54e1ff3c0
Ario Shakeri <itsario900@gmail.com>
2026-05-17 22:05:13 +0330

    fixed settings

--- scenes/settings_menu.gd
+++ scenes/settings_menu.gd
@@ -1,66 +1,50 @@
 class_name SettingsMenu
 extends CanvasLayer
 
-signal closed
-var ddd = preload("res://scenes/main_menu.tscn")
-var sdd = preload("res://scenes/piece_selection.tscn")
-var data := SettingsData.new()
-#@onready var btn_apply: Button = $PanelContainer/VBoxContainer/Buttons/ApplyButton
-#@onready var btn_reset: Button = $PanelContainer/VBoxContainer/Buttons/ResetButton
-#@onready var btn_back: Button = $PanelContainer/VBoxContainer/Buttons/BackButton
-#
-#func _ready() -> void:
-	##print(get_tree_string_pretty())
-	#btn_apply.pressed.connect(_on_apply)
-	#btn_reset.pressed.connect(_on_reset)
-	#btn_back.pressed.connect(_on_back)
-	#$Button/AnimatedSprite2D.play("default")
-#func _on_apply() -> void:
-	#SettingsManager.save_settings()
-#
-#func _on_reset() -> void:
-	#SettingsManager.reset_to_defaults()
-	#get_tree().reload_current_scene()
-#
-#func _on_back() -> void:
-	#SettingsManager.save_settings()
-	#closed.emit()
-	#queue_free()
-	#pass
-	#$Button/AnimatedSprite2D.play("new_animation")
+const CLICK_SFX = preload("res://assets/sound/فشردن دکمه های سنگی.mp3")
+const PANEL_OPEN_POS = Vector2(0, 0)
+const PANEL_CLOSED_POS = Vector2(0, 300)
+const ANIM_DURATION = 0.6
+
+@onready var content_panel: PanelContainer = $PanelContainer
+
+@onready var master_btn: VolumeSlider = $PanelContainer/VBoxContainer/MasterMute
+@onready var music_btn: VolumeSlider = $PanelContainer/VBoxContainer/MusicMute
+@onready var sfx_btn: VolumeSlider = $PanelContainer/VBoxContainer/SFXMute
+
+@onready var lang_menu: LanguagePicker = $PanelContainer/VBoxContainer/LanguagePicker
+
+var is_animating: bool = false
+
+func _ready() -> void:
+	hide()
+	content_panel.position = PANEL_CLOSED_POS
+	_sync_ui_with_settings()
+
+func _sync_ui_with_settings() -> void:
+	master_btn.check_button.button_pressed = not SettingsManager.data.master_muted
+	music_btn.check_button.button_pressed = not SettingsManager.data.music_muted
+	sfx_btn.check_button.button_pressed = not SettingsManager.data.sfx_muted
+
+func open() -> void:
+	show()
+	content_panel.modulate.a = 0
+	content_panel.position = PANEL_CLOSED_POS
 	
-func _on_check_button_pressed() -> void:
-	if ddd: 
-		if $Button/PanelContainer/VBoxContainer/Label2/CheckButton.button_pressed == false:  
-			AudioManager.stop_music()
-		else:
-			var sg = load("res://assets/sound/music_menu.ogg")
-			AudioManager.play_music(sg)
-	$Button/AnimatedSprite2D.play("hoverr")
+	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.tween_property(content_panel, "position", PANEL_OPEN_POS, ANIM_DURATION)
+	tween.tween_property(content_panel, "modulate:a", 1.0, ANIM_DURATION)
+
+func close() -> void:
+	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	tween.tween_property(content_panel, "position", PANEL_CLOSED_POS, ANIM_DURATION)
+	tween.tween_property(content_panel, "modulate:a", 0.0, ANIM_DURATION)
 	
-	SettingsManager.save_settings()
+	tween.chain().finished.connect(func(): hide())
 
+func _on_mute_toggled(toggled_on: bool, bus_name: String) -> void:
+	SettingsManager.set_muted(bus_name, toggled_on)
 
-func _on_button_mouse_exited() -> void:
-	$Button/AnimatedSprite2D.play("elseee")
-
-	#$"../../..".position.x = 483
-	#$"../../..".position.y = 210
-	#$"../..".position.x = 104
-	#$"../..".position.y = 63
-func _on_button_pressed() -> void:
-	AudioManager.play_sfx(preload("res://assets/sound/فشردن دکمه های سنگی.mp3"))
-	if $Button/PanelContainer.visible == false:
-		$Button/PanelContainer.visible = true
-		create_tween().tween_property($Button/PanelContainer, "position", Vector2(5, -150), 3).set_trans(Tween.TRANS_BACK)  
-		$Button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		await get_tree().create_timer(2.5).timeout
-		$Button.mouse_filter = Control.MOUSE_FILTER_PASS
-	else:
-		create_tween().tween_property($Button/PanelContainer, "position", Vector2(5, 82), 3).set_trans(Tween.TRANS_BACK)   
-		$Button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		await get_tree().create_timer(2).timeout
-		$Button/PanelContainer.visible = false
-		$Button.mouse_filter = Control.MOUSE_FILTER_PASS
-func _on_button_mouse_entered() -> void:
-	$Button/AnimatedSprite2D.play("hoverr")
+func _on_language_pressed(locale: String) -> void:
+	SettingsManager.set_locale(locale)
+	_sync_ui_with_settings()
--- scripts/settings_data.gd
+++ scripts/settings_data.gd
@@ -1,12 +1,12 @@
 class_name SettingsData
 extends Resource
 
-const DEFAULT_MASTER_VOLUME := 1.0
-const DEFAULT_MUSIC_VOLUME := 0.8
-const DEFAULT_SFX_VOLUME := 0.8
+const MASTER_MUTED := false
+const MUSIC_MUTED := false
+const SFX_MUTED := false
 const DEFAULT_LOCALE := "en"
 
-@export var master_volume: float  = DEFAULT_MASTER_VOLUME
-@export var music_volume: float  = DEFAULT_MUSIC_VOLUME
-@export var sfx_volume: float  = DEFAULT_SFX_VOLUME
+@export var master_muted: bool  = MASTER_MUTED
+@export var music_muted: bool  = MUSIC_MUTED
+@export var sfx_muted: bool  = SFX_MUTED
 @export var locale: String = DEFAULT_LOCALE
