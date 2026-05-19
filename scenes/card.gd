ab8e3a31e195beb5f00461693bac893ee35615ff
Ario Shakeri <itsario900@gmail.com>
2026-05-19 18:11:48 +0330

    reworked cards

--- scenes/card.gd
+++ scenes/card.gd
@@ -8,17 +8,14 @@ signal clicked(card: Card)
 @export var berserker_icon: Texture2D
 @export var util_icon: Texture2D
 
-@onready var blank_card: TextureRect = $BlankCard
-@onready var card_texture: TextureRect = $CardTexture
-@onready var selection_glow: Panel = get_node_or_null("SelectionGlow")
+@export_group("Stat Icons")
+@export var hp_icon: Texture2D
+@export var atk_icon: Texture2D
+@export var kb_icon: Texture2D
 
-@onready var name_label: Label = $BlankCard/Margin/Content/Header/NameLabel
-@onready var piece_texture: TextureRect = $BlankCard/Margin/Content/Header/PieceTexture
-@onready var class_icon_rect: TextureRect = $BlankCard/Margin/Content/ClassIcon
-@onready var class_icon_rect_front: TextureRect = $CardTexture/Panel/ClassIcon
-
-@onready var defense_bar: ProgressBar = $BlankCard/Margin/Content/Stats/DefenseBar
-@onready var power_bar: ProgressBar = $BlankCard/Margin/Content/Stats/PowerBar
+@onready var blank_card = $BlankCard
+@onready var card_texture = $CardTexture
+@onready var glow = $SelectionGlow
 
 var piece_data: PieceData
 var is_selected: bool = false
@@ -27,81 +24,59 @@ var original_scale: Vector2
 
 func _ready() -> void:
 	original_scale = scale
-	blank_card.visible = false
-	if selection_glow: selection_glow.visible = false
-	if piece_data:
-		_update_display()
+	blank_card.hide()
+	glow.hide()
 
 func set_piece_data(data: PieceData) -> void:
 	piece_data = data
-	if is_node_ready():
-		_update_display()
+	_update_display()
 
 func _update_display() -> void:
 	if not piece_data: return
 	
 	card_texture.texture = piece_data.card_texture
-	piece_texture.texture = piece_data.texture_white
-	name_label.text = piece_data.name
-	defense_bar.value = piece_data.defense * 10
-	power_bar.value = piece_data.power * 10
 	
+	$BlankCard/Margin/Content/NameLabel.text = piece_data.name.to_upper()
+	$BlankCard/Margin/Content/StatsGrid/HP_Row/Bar.value = piece_data.defense
+	$BlankCard/Margin/Content/StatsGrid/ATK_Row/Bar.value = piece_data.power
+	$BlankCard/Margin/Content/StatsGrid/KB_Row/Bar.value = piece_data.knockback
+	
+	$BlankCard/Margin/Content/StatsGrid/HP_Row/Icon.texture = hp_icon
+	$BlankCard/Margin/Content/StatsGrid/ATK_Row/Icon.texture = atk_icon
+	$BlankCard/Margin/Content/StatsGrid/KB_Row/Icon.texture = kb_icon
+
+	var c_icon = _get_class_icon()
+	$BlankCard/Margin/Content/ClassIcon.texture = c_icon
+
+func _get_class_icon() -> Texture2D:
 	match piece_data.piece_class:
-		PieceData.PieceClass.TANK:
-			class_icon_rect.texture = tank_icon
-		PieceData.PieceClass.BERSERKER:
-			class_icon_rect.texture = berserker_icon
-		PieceData.PieceClass.UTILITY:
-			class_icon_rect.texture = util_icon
-	
-	class_icon_rect_front.texture = class_icon_rect.texture
+		PieceData.PieceClass.TANK: return tank_icon
+		PieceData.PieceClass.BERSERKER: return berserker_icon
+		PieceData.PieceClass.UTILITY: return util_icon
+	return null
 
 func select() -> void:
-	if is_selected: return
 	is_selected = true
-	
-	_animate_selection(true)
-	AudioManager.play_sfx(preload("res://assets/sound/فشردن دکمه های سنگی.mp3"))
+	glow.show()
+	create_tween().tween_property(self, "scale", original_scale * 1.05, 0.1)
 
 func deselect() -> void:
-	if not is_selected: return
 	is_selected = false
-	
-	_animate_selection(false)
-
-func _animate_selection(should_select: bool) -> void:
-	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	
-	if should_select:
-		tween.tween_property(self, "scale", original_scale * 1.075, 0.2)
-		tween.tween_property(self, "modulate", Color(1.2, 1.2, 1.1), 0.2)
-		if selection_glow: selection_glow.show()
-	else:
-		tween.tween_property(self, "scale", original_scale, 0.2)
-		tween.tween_property(self, "modulate", Color.WHITE, 0.2)
-		if selection_glow: selection_glow.hide()
-
-func set_disabled(value: bool) -> void:
-	is_disabled = value
-	modulate = Color(0.3, 0.3, 0.3, 0.8) if value else Color.WHITE
-	mouse_default_cursor_shape = Control.CURSOR_ARROW if value else Control.CURSOR_POINTING_HAND
-
-func _gui_input(event: InputEvent) -> void:
-	if is_disabled: return
-	
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			clicked.emit(self)
-			accept_event()
+	glow.hide()
+	create_tween().tween_property(self, "scale", original_scale, 0.1)
 
 func _on_mouse_entered() -> void:
 	if is_disabled: return
-	card_texture.visible = false
-	blank_card.visible = true
-	create_tween().tween_property(self, "position:y", position.y - 5, 0.1)
+	card_texture.hide()
+	blank_card.show()
 
 func _on_mouse_exited() -> void:
 	if is_disabled: return
-	card_texture.visible = true
-	blank_card.visible = false
-	create_tween().tween_property(self, "position:y", position.y + 5, 0.1)
+	card_texture.show()
+	blank_card.hide()
+
+func _gui_input(event: InputEvent) -> void:
+	if is_disabled: return
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		clicked.emit(self)
+		accept_event()
