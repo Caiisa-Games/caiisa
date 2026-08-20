6cb73b00c292db3e976d513d13aed624edd4c400
Ario Shakeri <itsario900@gmail.com>
2026-08-21 03:26:44 +0330

    Add passive abilities

--- scripts/abilities/passive/knight.gd
+++ scripts/abilities/passive/knight.gd
@@ -1,5 +1,5 @@
 class_name KnightPassive
 extends PassiveEffect
 
-static func allows_jump_over(piece_data: PieceData) -> bool:
-	return piece_data and piece_data.name == "knight"
+func allows_jump_over() -> bool:
+	return true
